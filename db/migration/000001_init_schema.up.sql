381332d55c89f822b5372e94dd004090c13f28e1
petrostrak <pit.trak@gmail.com>
2023-05-23 16:53:18 +0300

    Remove unnecessary sql statements

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -16,11 +16,3 @@ CREATE TABLE "transfers" (
 ALTER TABLE "transfers" ADD FOREIGN KEY ("source_account_id") REFERENCES "accounts" ("id");
 
 ALTER TABLE "transfers" ADD FOREIGN KEY ("target_account_id") REFERENCES "accounts" ("id");
-
-CREATE INDEX ON "transfers" ("source_account_id");
-
-CREATE INDEX ON "transfers" ("target_account_id");
-
-CREATE INDEX ON "transfers" ("source_account_id", "target_account_id");
-
-COMMENT ON COLUMN "accounts"."balance" IS 'must always be positive';
