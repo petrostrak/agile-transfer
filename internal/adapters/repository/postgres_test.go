77d02fba7e8ecea23858883712d491dde6fb6cfd
petrostrak <pit.trak@gmail.com>
2023-05-28 12:22:25 +0300

    Add Test_PostgresDBRepoInsertAccount

--- internal/adapters/repository/postgres_test.go
+++ internal/adapters/repository/postgres_test.go
@@ -6,10 +6,13 @@ import (
 	"log"
 	"os"
 	"testing"
+	"time"
 
 	_ "github.com/lib/pq"
 	"github.com/ory/dockertest/v3"
 	"github.com/ory/dockertest/v3/docker"
+	"github.com/petrostrak/agile-transfer/internal/core/domain"
+	"github.com/shopspring/decimal"
 )
 
 var (
@@ -25,6 +28,7 @@ var (
 	resource *dockertest.Resource
 	pool     *dockertest.Pool
 	testDB   *sql.DB
+	testRepo PostgresRepository
 )
 
 func TestMain(m *testing.M) {
@@ -75,6 +79,11 @@ func TestMain(m *testing.M) {
 		log.Fatalf("error creating tables: %s", err)
 	}
 
+	testRepo = PostgresRepository{
+		&AccountRepository{testDB},
+		&TransferRepository{testDB},
+	}
+
 	code := m.Run()
 
 	if err := pool.Purge(resource); err != nil {
@@ -104,5 +113,18 @@ func Test_PingDB(t *testing.T) {
 	err := testDB.Ping()
 	if err != nil {
 		t.Error("cannot ping DB")
+	}
+}
+
+func Test_PostgresDBRepoInsertAccount(t *testing.T) {
+	testAccount := domain.Account{
+		Balance:   decimal.NewFromInt(150000),
+		Currency:  "EUR",
+		CreatedAt: time.Now(),
+	}
+
+	err := testRepo.AccountRepository.Insert(&testAccount)
+	if err != nil {
+		t.Errorf("insert account returned an error: %s", err)
 	}
 }
