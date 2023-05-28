85d53b50294d354f57ed57d0649ff0d0b72c4f91
petrostrak <pit.trak@gmail.com>
2023-05-28 11:09:59 +0300

    Spin docker image for tests

--- internal/adapters/repository/postgres_test.go
+++ internal/adapters/repository/postgres_test.go
@@ -2,11 +2,14 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
 	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 )
 
 var (
@@ -25,6 +28,46 @@ var (
 )
 
 func TestMain(m *testing.M) {
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("could not connect to docker: %s", err)
+	}
+
+	pool = p
+
+	options := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "14",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: port},
+			},
+		},
+	}
+	resource, err := pool.RunWithOptions(&options)
+	if err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not start resource: %s", err)
+	}
+
+	if err := pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("postgres", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			log.Println("error:", err)
+			return err
+		}
+		return testDB.Ping()
+	}); err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not connect to DB: %s", err)
+	}
 
 	code := m.Run()
 	os.Exit(code)
